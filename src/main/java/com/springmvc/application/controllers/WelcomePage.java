5c78eda8f807aa45725524ba75266d10564268fd
Karan Partap Singh <karanpar@amazon.com>
2018-08-25 17:13:45 +0530

    Using customDateEditor class to edit date in my defined Format

--- src/main/java/com/springmvc/application/controllers/WelcomePage.java
+++ src/main/java/com/springmvc/application/controllers/WelcomePage.java
@@ -1,8 +1,11 @@
 package com.springmvc.application.controllers;
 
 import com.springmvc.application.helpers.Student;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +13,22 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class WelcomePage {
+
+    /**
+     * How the binding can be customised using initBinder annotation and also using WebDataBinder
+     */
+    @InitBinder
+    public void intBinder(WebDataBinder binder) {
+        binder.setDisallowedFields(new String[] {"studentMobile"});         //  the mobile number will not be binded
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy****MM****dd");     // allows customization of the dates format  <---- This is custom property editor classes
+        binder.registerCustomEditor(Date.class, "studentDOB", new CustomDateEditor(dateFormat, false));
+    }
+
 
     @RequestMapping(value = "/addmissionForm", method = RequestMethod.GET)
     public ModelAndView admissionFormPage(final HttpServletRequest request, final HttpServletResponse response) {
